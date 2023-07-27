b09d6cace09d2c2c00dab51ed65d8481f9c1191e
sattya19 <sattyasharma195@gmail.com>
2023-07-27 21:50:51 +0530

    addedMoviePage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import MovieCard from "./components/MovieCard";
+// import SearchIcon from "search.svg";
 
-function App() {
+
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
   return (
-    <>
+    <div className="app">
+      <h1>CHILLS</h1>
 
+      <div onClick={() => searchMovies(searchTerm)} className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        {<img
+          src={<svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M29.8594 29.8594L39.4219 39.4219" stroke="#D88769" stroke-width="4.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M17.9062 33.0469C26.2682 33.0469 33.0469 26.2682 33.0469 17.9062C33.0469 9.54431 26.2682 2.76562 17.9062 2.76562C9.54431 2.76562 2.76562 9.54431 2.76562 17.9062C2.76562 26.2682 9.54431 33.0469 17.9062 33.0469Z" stroke="#D88769" stroke-width="4.5" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          }
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />}
+      </div>
 
-    </>
-  
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
